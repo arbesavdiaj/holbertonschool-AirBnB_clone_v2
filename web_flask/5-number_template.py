6c17cdffec12c2b5f060917bb5a09b418102988b
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-04-08 13:08:49 +0200

    fix code

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,25 +1,32 @@
-from flask import Flask, render_template # type: ignore
+#!/usr/bin/python3
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def index():
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
     text = text.replace('_', ' ')
     return 'C ' + text
 
+
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text='is cool'):
     text = text.replace('_', ' ')
     return 'Python ' + text
+
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
@@ -28,12 +35,14 @@ def number(n):
     else:
         return 'Not Found', 404
 
+
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     if isinstance(n, int):
         return render_template('number_template.html', number=n)
     else:
         return 'Not Found', 404
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
