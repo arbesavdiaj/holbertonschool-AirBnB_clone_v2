e06ba8f4df14e5451fee3bb6d4d73390ad415936
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-04-08 13:09:50 +0200

    fix pycode

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,25 +1,32 @@
-from flask import Flask, render_template # type: ignore
+#!/usr/bin/python3
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def index():
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
     text = text.replace('_', ' ')
     return 'C ' + text
 
+
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text='is cool'):
     text = text.replace('_', ' ')
     return 'Python ' + text
+
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
@@ -28,6 +35,7 @@ def number(n):
     else:
         return 'Not Found', 404
 
+
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     if isinstance(n, int):
@@ -35,13 +43,16 @@ def number_template(n):
     else:
         return 'Not Found', 404
 
+
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
     if isinstance(n, int):
         odd_or_even = 'odd' if n % 2 != 0 else 'even'
-        return render_template('number_odd_or_even.html', number=n, odd_or_even=odd_or_even)
+        return render_template('number_odd_or_even.html',
+                               number=n, odd_or_even=odd_or_even)
     else:
         return 'Not Found', 404
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
