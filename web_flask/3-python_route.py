ab6df34164814d26790982ecffaef70c176ad081
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-04-08 13:07:07 +0200

    fix pycodestyle

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,24 +1,32 @@
-from flask import Flask # type: ignore
+#!/usr/bin/python3
+
+
+from flask import Flask
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def index():
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
     text = text.replace('_', ' ')
     return 'C ' + text
 
+
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text='is cool'):
     text = text.replace('_', ' ')
     return 'Python ' + text
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
