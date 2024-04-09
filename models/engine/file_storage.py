4c2a05a91ecb7939646e72abcefd3c6cef91074c
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-04-09 09:26:29 +0200

    add new method close

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -18,6 +18,10 @@ class FileStorage:
             return cls_dict
         return FileStorage.__objects
 
+    def close(self):
+        """method for deserializing the JSON file to objects"""
+        self.reload()
+
     def new(self, obj):
         """Adds new object to storage dictionary"""
         self.all().update({obj.to_dict()["__class__"] + "." + obj.id: obj})
