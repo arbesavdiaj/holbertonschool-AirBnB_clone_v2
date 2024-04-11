2ef76f116a2fc36b9fd522195efa956acff71188
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-04-11 10:34:37 +0200

    add comments to number route

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+add number route
+"""
 
 from flask import Flask
 app = Flask(__name__)
@@ -6,28 +9,43 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
+    """
+    return Hello HBNB!
+    """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """
+    return HBNB
+    """
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
+    """
+    return C + text
+    """
     text = text.replace('_', ' ')
     return 'C ' + text
 
 
 @app.route('/python/<text>', strict_slashes=False)
 def python(text):
+    """
+    return python + text
+    """
     text = text.replace('_', ' ')
     return 'Python ' + text
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
+    """
+    return number + int
+    """
     if isinstance(n, int):
         return f'{n} is a number'
     else:
