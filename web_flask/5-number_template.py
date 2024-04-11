e2cf985a06105722cd333a621e98ab78ed781489
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-04-11 10:40:57 +0200

    add new comments

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 """
-number template with html
+Module to initiate a flask app
 """
 
-from flask import Flask, render_template
+
+from flask import Flask
+from flask import render_template
+
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index():
+def hello_hbnb():
     """
-    return Hello HBNB!
+    Index route
     """
     return 'Hello HBNB!'
 
@@ -19,51 +22,44 @@ def index():
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """
-    return HBNB
+    HBNB route
     """
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c(text):
+def c_is_fun(text):
     """
-    return C + text
+    Dynamic routing
     """
-    text = text.replace('_', ' ')
-    return 'C ' + text
+    return 'C ' + text.replace('_', ' ')
 
 
-@app.route('/python/', strict_slashes=False)
+@app.route('/python/')
 @app.route('/python/<text>', strict_slashes=False)
-def python(text='is cool'):
+def python_is_cool(text='is cool'):
     """
-    return python + text
+    Dynamic routing
     """
-    text = text.replace('_', ' ')
-    return 'Python ' + text
+    return 'Python ' + text.replace('_', ' ')
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
+def is_number(n):
     """
-    return number + int
+    Dynamic routing
     """
-    if isinstance(n, int):
-        return f'{n} is a number'
-    else:
-        return 'Not Found', 404
+    return str(n) + ' is a number'
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     """
-    return number template + int
+    Dynamic routing and template rendering
     """
-    if isinstance(n, int):
-        return render_template('number_template.html', n=n)
-    else:
-        return 'Not Found', 404
+    return render_template('5-number.html', n=n)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
