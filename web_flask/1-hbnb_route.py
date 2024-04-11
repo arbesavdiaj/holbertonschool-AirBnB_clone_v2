584f24a4cf7e8c032f8d180090bf271e0292add7
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-04-11 10:28:39 +0200

    add new comments

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+hbnb route
+"""
 
 from flask import Flask
 app = Flask(__name__)
@@ -6,11 +9,17 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
+    """
+    return HBNB!
+    """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """
+    return only HBNB
+    """
     return 'HBNB'
 
 
