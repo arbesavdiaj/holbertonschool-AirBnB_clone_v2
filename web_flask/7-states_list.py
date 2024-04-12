61ede5bce12513fd2722ae6f424068bf0286c474
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-04-12 19:36:02 +0200

    add new code

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """
-Creating an app
+Script that starts a Flask web application
 """
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
 from models import storage
 from models.state import State
 
@@ -11,9 +12,9 @@ app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def close_db(error):
+def tear_db(exe):
     """
-    Simple function
+    Close storage on teardown
     """
     storage.close()
 
@@ -23,12 +24,9 @@ def states_list():
     """
     dynamic routing
     """
-    all_states = [{'id': value.to_dict()['id'],
-                   'state': value.to_dict()['name']}
-                  for value in storage.all(State).values()]
-    all_states = sorted(all_states, key=lambda state: state['state'])
-    return render_template('7-states_list.html', all_states=all_states)
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(host='0.0.0.0', port='5000')
